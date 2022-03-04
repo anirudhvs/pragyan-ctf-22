e7fb3f3c3a21f11dc8cc9aa93fbd9279455393b4
Dhanush T <83498774+Dhanush-T@users.noreply.github.com>
2022-03-04 17:53:24 +0530

    fix: removed csrf middleware (#52)

--- Web/LostFlag/includes/lostflag/lostflag/settings.py
+++ Web/LostFlag/includes/lostflag/lostflag/settings.py
@@ -23,10 +23,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'lostflag.lostflag_middleware.SimpleMiddleware'
 ]
 
@@ -79,14 +77,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-# STATIC_URL = '/static/'
-# if DEBUG:
-#     STATICFILES_DIRS = [
-#         os.path.join(BASE_DIR, 'static'),
-#     ]
-# else:
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
